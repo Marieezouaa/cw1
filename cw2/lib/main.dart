a42423561da37bea33b83201fc6a4f9fd26967c5
Marie Ezoua <marieezoua@Maries-MacBook-Pro.local>
2024-09-11 16:51:55 -0400

    This repo is the second part of the cw assignment

---
+++ cw2/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:cw2/styles.dart';
+void main() {
+  runApp( const MaterialApp(
+      home: Scaffold(
+        body: Styles(
+            Color.fromRGBO(143, 264, 198, 1), Color.fromRGBO(84, 116, 207, 1)),
+      ),
+    ),);
+}
